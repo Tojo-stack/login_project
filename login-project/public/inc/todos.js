da5156073998f9ed6c1520176d6fa71974887389
tojo-stack <toumandimbison@gmail.com>
2025-09-19 20:24:38 +0100

    Fix todo list AJAX: ajout, suppression et marquer comme fait

--- login-project/public/inc/todos.js
+++ login-project/public/inc/todos.js
@@ -6,15 +6,12 @@ async function loadTodos() {
         headers: {'Accept' : 'application/json'}
     });
 
-    console.log('Réponse fectch', res)
-
     if (!res.ok) {
         console.error('Erreur fetch todos', res.status);
         return;
     }
-    console.log('Réponse fetch: ', res)
+
     const todos = await res.json();
-    console.log('Todos reçus: ', todos)
     const list = document.getElementById('todo-list');
     list.innerHTML = ''
 
@@ -22,19 +19,57 @@ async function loadTodos() {
         const li = document.createElement('li');
         li.innerHTML = `
             ${todo.done ? "<s>" + todo.task + "</s>" : todo.task}
-            <form style="display:inline" action="/todos/${todo.id}/done" method="POST">
-                <button type="submit">Fait</button>
-            </form>
-            <form style="display:inline" action="/todos/${todo.id}/delete" method="POST">
-                <button type="submit">Supprimer</button>
-            </form>
+            <button class="done-btn" data-id="${todo.id}" style="">Fait</button>
+            <button class="delete-btn" data-id="${todo.id}">Supprimer</button>
         `;
         list.appendChild(li);
     });
+
+    // ... Gestion des clics 'Fait' ...
+    document.querySelectorAll('.done-btn').forEach(btn => {
+            btn.addEventListener('click', async () => {
+                const id = btn.dataset.id
+                const res = await fetch(`/todos/${id}/done`, { method: 'POST' })
+                if (res.ok) loadTodos()
+            })
+        })
+
+        // ... Gestion des clics 'Supprimer' ...
+    document.querySelectorAll('.delete-btn').forEach(btn => {
+            btn.addEventListener('click', async () => {
+                const id = btn.dataset.id
+                const res = await fetch(`/todos/${id}/delete`, { method: 'POST' })
+                if (res.ok) loadTodos()
+            })
+        })
+
+
     }catch (err){
         console.error ('Erreur fetch todos: ', err)
     }
     
 }
+
+
+
+//  ... Ajouter une tâche ...
+document.getElementById('add-form').addEventListener('submit', async (e) =>{
+    e.preventDefault()
+    const taskInput = document.getElementById('task-input').value.trim()
+    if (!taskInput) return
+
+    const res = await fetch('/todos', {
+        method: 'POST',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify({task: taskInput})
+    })
+
+    if(res.ok){
+        document.getElementById('task-input').value = ''
+        loadTodos()
+    }else{
+        console.error('Erreur ajout todo', res.status)
+    }
+})
 
 loadTodos();
